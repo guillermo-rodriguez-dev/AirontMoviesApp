5a446696c8c0bb78e1b1244ad9b502195dec5797
Guillermo L. Rodriguez <glrodriguez.dev@gmail.com>
2022-11-13 14:57:18 -0300

    3/se agrega navbar

---
+++ js/login.js
@@ -0,0 +1,106 @@
+
+
+async function signUp(event) {
+    event.preventDefault();
+    let loginForm = document.getElementById("loginForm");
+    let email = loginForm.email.value;
+    let password = loginForm.password.value;
+
+    if (email == "") {
+        alert("Please enter your email");
+        return false;
+    }
+    if (email.indexOf("@") == -1) {
+        alert("Please enter a valid email");
+        return false;
+    }
+    if (password == "") {
+        alert("Please enter your password");
+        return false;
+    }
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters");
+        return false;
+    }
+
+    await fetch("http://localhost:3000/users", {
+        method: "POST",
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+    }).then(response => {
+        if (response.status == 201) {
+            alert("User created successfully");
+            window.location.href = "login.html";
+
+        }
+        else if (response.status == 400) {
+            alert(response.body);
+        }
+
+    }
+
+
+    ).catch(error => console.log("Error " + error));
+
+
+}
+
+
+
+
+async function signIn(event) {
+    event.preventDefault();
+    let loginForm = document.getElementById("loginForm");
+    let email = loginForm.email.value;
+    let password = loginForm.password.value;
+
+    if (email == "") {
+        alert("Please enter your email");
+        return false;
+    }
+    if (email.indexOf("@") == -1) {
+        alert("Please enter a valid email");
+        return false;
+    }
+    if (password == "") {
+        alert("Please enter your password");
+        return false;
+    }
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters");
+        return false;
+    }
+
+    await fetch("http://localhost:3000/users", {
+        method: "GET",
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+    }).then(response => {
+        if (response.status == 200) {
+            alert("User logged in successfully");
+            window.location.href = "login.html";
+
+        }
+        else if (response.status == 400) {
+            alert(response.body);
+        }
+
+    }
+
+
+    ).catch(error => console.log("Error " + error));
+
+
+}
