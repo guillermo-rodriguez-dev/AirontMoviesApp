024caa9c5402a773d04ef8d91c769f43532533f9
Guillermo L. Rodriguez <glrodriguez.dev@gmail.com>
2022-11-20 21:57:07 -0300

    se agrega api de most watched movies

--- js/apiconnection.js
+++ js/apiconnection.js
@@ -1,5 +1,62 @@
 const apiKey = "51a36c9caed1e09d8398a600b96856cc";
 const apiBaseUrl = "https://api.themoviedb.org/3";
+const movieContainer = document.getElementById("most-watched-movies-container");
+
+
+const getMoviesFromApi = async () => {
+    const response = await fetch(`${apiBaseUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
+    const data = await response.json();
+    const movies = data.results;
+    return movies;
+}
 
 
 
+const addMostWatchesMovies = async () => {
+    const movies = await getMoviesFromApi();
+    for (let index = 0; index < 3; index++) {
+        const movie = document.createElement("div");
+        movie.classList.add("movie");
+        movie.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movies[index].poster_path})`;
+        const movieTitle = document.createElement("h3");
+        movieTitle.classList.add("most-watched-movie-title");
+        movieTitle.textContent = movies[index].title;
+        movie.appendChild(movieTitle);
+        const ratingContainer = document.createElement("div");
+        ratingContainer.classList.add("rating-container");
+        const ratingStar = document.createElement("i");
+        ratingStar.classList.add("fa", "fa-star", "rating-start--small");
+        ratingContainer.appendChild(ratingStar);
+        movie.appendChild(ratingContainer);
+        const movieDescription = document.createElement("p");
+        movieDescription.classList.add("most-watched-movie-description");
+        movieDescription.textContent = movies[index].overview;
+        movie.appendChild(movieDescription);
+        movieContainer.appendChild(movie)
+    }
+    return data;
+}
+
+
+
+
+
+
+addMostWatchesMovies();
+
+
+{/* <div class="movie">
+
+    <h3 class="most-watched-movie-title">Godzilla vs Kong</h3>
+    <div class="rating-container">
+        <i class="fa fa-star rating-start--small" alt="rating start"></i>
+        <i class="fa fa-star rating-start--small" alt="rating start"></i>
+        <i class="fa fa-star rating-start--small" alt="rating start"></i>
+        <i class="fa fa-star rating-start--small" alt="rating start"></i>
+        <i class="fa fa-star rating-start--small" alt="rating start"></i>
+    </div>
+    <p class="most-watched-movie-description">Five years after Godzilla defeated King Ghidorah, Kong is
+        monitored by Monarch within a giant dome on
+        Skull Island.
+    </p>
+</div> */}
