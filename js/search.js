bf4b108af8c8dd4ad7022bd7504c1f0073d8affa
Guillermo L. Rodriguez <glrodriguez.dev@gmail.com>
2022-11-26 15:33:56 -0300

    se agrega resultados de busqueda

--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 import constants from "./constants.js";
 const searchbarInput = document.getElementById('searchbar-input');
 const searchIconButton = document.getElementById('searchbar-icon');
+const searchResultContainer = document.getElementById("search-result")
 
 const searchMovieByTitle = async (title) => {
     const response = await fetch(`${constants.apiBaseUrl}/search/movie?api_key=${constants.apiKey}&language=en-US&query=${title}&page=1&include_adult=false`);
@@ -26,12 +27,25 @@ searchbarInput.addEventListener('keyup', async (event) => {
 
 });
 
-searchbarInput.addEventListener('keypress', async (event) => {
-   
+searchbarInput.addEventListener('keydown', async (event) => {
+    searchResultContainer.innerHTML = "";
+    if (searchbarInput.value.length > 2) {
         const movies = await searchMovieByTitle(searchbarInput.value);
-        console.log(movies);
-    
 
+        movies.forEach(element => {
+            const movieResult = document.createElement("li");
+            const movieImage = document.createElement("img");
+            movieImage.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`;
+            movieResult.appendChild(movieImage);
+            movieResult.innerHTML += element.title;
+            searchResultContainer.appendChild(movieResult);
+        });
 
-
+    } else {
+        searchResultContainer.innerHTML = "";
+    }
 });
+
+{/* <li><img src="../images/avatar.png"  /> Prueba    </li>
+
+</ul> */}
